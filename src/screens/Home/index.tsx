78542065ebce3f36048199e8cdddcc7d8da0c510
Carlos Silva <carlosteixeira.tc@hotmail.com>
2021-10-30 20:50:42 -0300

    save dados Async storage

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,10 +1,10 @@
-import React, { useState, useCallback } from 'react';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { useFocusEffect } from '@react-navigation/native';
+import React, { useState, useCallback, useEffect } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useFocusEffect } from "@react-navigation/native";
 
-import { Header } from '../../components/Header';
-import { SearchBar } from '../../components/SearchBar';
-import { LoginDataItem } from '../../components/LoginDataItem';
+import { Header } from "../../components/Header";
+import { SearchBar } from "../../components/SearchBar";
+import { LoginDataItem } from "../../components/LoginDataItem";
 
 import {
   Container,
@@ -12,7 +12,7 @@ import {
   Title,
   TotalPassCount,
   LoginList,
-} from './styles';
+} from "./styles";
 
 interface LoginDataProps {
   id: string;
@@ -24,33 +24,45 @@ interface LoginDataProps {
 type LoginListDataProps = LoginDataProps[];
 
 export function Home() {
-  const [searchText, setSearchText] = useState('');
+  const [searchText, setSearchText] = useState("");
   const [searchListData, setSearchListData] = useState<LoginListDataProps>([]);
   const [data, setData] = useState<LoginListDataProps>([]);
 
   async function loadData() {
-    const dataKey = '@savepass:logins';
-    // Get asyncStorage data, use setSearchListData and setData
+    const dataKey = "@savepass:logins";
+    const dados = await AsyncStorage.getItem(dataKey);
+    if (dados) {
+      const parsedDados = JSON.parse(dados);
+      setSearchListData(parsedDados);
+      setData(parsedDados);
+    }
   }
 
   function handleFilterLoginData() {
-    // Filter results inside data, save with setSearchListData
+    const filteredData = data.filter((item) => {
+      const itemData = item.service_name.toUpperCase();
+      const textData = searchText.toUpperCase();
+      return itemData.indexOf(textData) > -1;
+    });
+    setSearchListData(filteredData);
   }
 
   function handleChangeInputText(text: string) {
-    // Update searchText value
+    setSearchText(text);
   }
 
-  useFocusEffect(useCallback(() => {
-    loadData();
-  }, []));
+  useFocusEffect(
+    useCallback(() => {
+      loadData();
+    }, [])
+  );
 
   return (
     <>
       <Header
         user={{
-          name: 'Rocketseat',
-          avatar_url: 'https://i.ibb.co/ZmFHZDM/rocketseat.jpg'
+          name: "Rocketseat",
+          avatar_url: "https://i.ibb.co/ZmFHZDM/rocketseat.jpg",
         }}
       />
       <Container>
@@ -60,7 +72,6 @@ export function Home() {
           value={searchText}
           returnKeyType="search"
           onSubmitEditing={handleFilterLoginData}
-
           onSearchButtonPress={handleFilterLoginData}
         />
 
@@ -68,24 +79,24 @@ export function Home() {
           <Title>Suas senhas</Title>
           <TotalPassCount>
             {searchListData.length
-              ? `${`${searchListData.length}`.padStart(2, '0')} ao total`
-              : 'Nada a ser exibido'
-            }
+              ? `${`${searchListData.length}`.padStart(2, "0")} ao total`
+              : "Nada a ser exibido"}
           </TotalPassCount>
         </Metadata>
-
         <LoginList
           keyExtractor={(item) => item.id}
           data={searchListData}
           renderItem={({ item: loginData }) => {
-            return <LoginDataItem
-              service_name={loginData.service_name}
-              email={loginData.email}
-              password={loginData.password}
-            />
+            return (
+              <LoginDataItem
+                service_name={loginData.service_name}
+                email={loginData.email}
+                password={loginData.password}
+              />
+            );
           }}
         />
       </Container>
     </>
-  )
+  );
 }
